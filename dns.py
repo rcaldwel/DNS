575416957a39649cdcd3a89a3555233cab1c02cc
roger caldwell <roger.caldwell@apple.com>
2019-01-17 11:30:37 -0600

    fix

--- dns.py
+++ dns.py
@@ -4,30 +4,32 @@ import sublime, sublime_plugin
 class HostLookupCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         from socket import gethostbyaddr
-
+        print("host lookup command")
         sublime.status_message('getting hostnames....')
         for region in self.view.sel():
             try:
-                for line in region:
-                    if not region.empty():
-                        text = self.view.substr(region)
-                        result = gethostbyaddr(text)[0]
-                        self.view.replace(edit, region, result)
-            except Exception:
+                if not region.empty():
+                    text = self.view.substr(region)
+                    result = gethostbyaddr(text)[0]
+                    self.view.replace(edit, region, result)
+            except Exception as error:
+                print(str(error))
+                sublime.status_message("oops.  check console")
                 continue
 
 
 class NameLookupCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         from socket import gethostbyname
-
+        print("name lookup command")
         sublime.status_message('getting ip\'s.....')
         for region in self.view.sel():
             try:
-                for line in region:
-                    if not region.empty():
-                        text = self.view.substr(region)
-                        result = gethostbyname(text)
-                        self.view.replace(edit, region, result)
-            except Exception:
+                if not region.empty():
+                    text = self.view.substr(region)
+                    result = gethostbyname(text)
+                    self.view.replace(edit, region, result)
+            except Exception as error:
+                print(str(error))
+                sublime.status_message("oops.  check console")
                 continue
